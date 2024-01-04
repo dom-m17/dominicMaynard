29b1d520072944925a1f3b4a7905850c0a28d166
dom-m17 <dominicmaynard00@gmail.com>
2024-01-04 21:20:26 +0000

    finishing api calls

--- Task/js/apiCalls/oceans.js
+++ Task/js/apiCalls/oceans.js
@@ -3,11 +3,9 @@ export default async function getOcean(params) {
     const { lat, lng } = params
 
     try{
-        fetch(`http://api.geonames.org/oceanJSON?lat=${lat}&lng=${lng}&username=dom_m17`)
-        .then(response => response.json())
-        .then(data => {
-            return data
-        })
+        const response = await fetch(`http://api.geonames.org/oceanJSON?lat=${lat}&lng=${lng}&username=dom_m17`)
+        const data = await response.json()
+        return data
 
     } catch (error) {
             console.log('Error making API call:', error);
