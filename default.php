8e7e01ae27d49552a8cdb1969be4486042c27c4a
dom-m17 <123015468+dom-m17@users.noreply.github.com>
2024-01-05 20:05:29 +0000

    Add files via upload

---
+++ default.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+
+  <title>Dom's Portfolio Site</title>
+
+  <link href="style.css" rel="stylesheet">
+  <script src="script.js"></script>
+
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li><a href="./task/index.html">Task</a></li>
+        </ul>
+    </nav>
+
+    <header>
+        <section class="home">
+            <div class="home-items">Hi. I'm Dominic and this is my portfolio website.</div>
+            <img class="home-items" src="./resources/dom.jpeg" alt="Profile Image of Dominic">
+        </section>
+    </header>
+
+    <main>
+        <section class="projects">
+            <div>Project 1</div>
+            <div>Project 2</div>
+            <div>Project 3</div>
+        </section>
+    </main>
+
+</body>
+
+</html>
