8671953e2a39f87f725747980051da24ca455292
dom-m17 <dominicmaynard00@gmail.com>
2024-01-10 21:12:16 +0000

    Change task to Task

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <a href="/task/">Task</a>
+    <a href="/Task/">Task</a>
     <a href="/src/vercelFile/">Vercel</a>
     <a href="/src/vercelFile/alternate.html">Alternate</a>
   </React.StrictMode>,
