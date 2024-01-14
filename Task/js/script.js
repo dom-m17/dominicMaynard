23d065243042b8bfbf8131c11c25bc899b1b0215
dom-m17 <dominicmaynard00@gmail.com>
2024-01-14 14:57:36 +0000

    Completing Oceans request

--- Task/js/script.js
+++ Task/js/script.js
@@ -1,21 +1,24 @@
-import { ajax } from 'jquery';
-
-$('#ocean-btn').click(function()  {
+$('#ocean-btn').click(function() {
     $.ajax({
-        url: "/Task/php/oceans.php",
-        type: "POST",
-        datatype: "json",
+        url: "php/oceans.php",
+        type: 'POST',
+        dataType: 'json',
         data: {
-            lat: document.getElementById("ocean-lat").value,
-            lng: document.getElementById("ocean-lng").value
+            lat: $('#ocean-lat').val(),
+            lng: $('#ocean-lng').val(),
         },
         success: function(result) {
-            console.log(result)
+                console.log(result);
+                console.log("Success")
+				$('#ocean-geoname-id').html(result.data.ocean.geonameId);
+				$('#ocean-name').html(result.data.ocean.name);
+				$('#ocean-distance').html(result.data.ocean.distance);
         },
-        error: function() {
-            console.log("There was an error.")
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(`Error`);
+            $('#ocean-geoname-id').html("There is no ocean at those coordinates");
+            $('#ocean-name').html("");
+            $('#ocean-distance').html("");
         }
-    }
-    
-    )
-})
+    }); 
+});
--- Task/php/oceans.php
+++ Task/php/oceans.php
@@ -1,17 +1,19 @@
 <?php
-    $geonameKey = getenv('GEONAMES_USERNAME');
 
-    $url = 'http://api.geonames.org/oceanJSON?formatted=true&lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=' . $geonameKey;
+    $executionStartTime = microtime(true);
+
+
+    $url='http://api.geonames.org/oceanJSON?lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=' . 'dom_m17' . '&style=full';
     $ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
 
     $result=curl_exec($ch);
+    curl_close($ch);
 
-	curl_close($ch);
+    $decode = json_decode($result,true);	
 
-	$decode = json_decode($result,true);
 
     $output['status']['code'] = "200";
     $output['status']['name'] = "ok";
@@ -24,19 +26,19 @@
 
     echo json_encode($output); 
 
+    // function getOcean($params) {
+    //     $lat = $params['lat'];
+    //     $lng = $params['lng'];
+    
+    //     try {
+    //         $url = "http://api.geonames.org/oceanJSON?lat={$lat}&lng={$lng}&username=dom_m17";
+    //         $response = file_get_contents($url);
+    //         $data = json_decode($response, true);
+    
+    //         return $data;
+    //     } catch (Exception $error) {
+    //         echo 'Error making API call: ' . $error->getMessage();
+    //     }
+    // }
+
 ?>
-
-<!-- function getOcean($params) {
-    $lat = $params['lat'];
-    $lng = $params['lng'];
-
-    try {
-        $url = "http://api.geonames.org/oceanJSON?lat={$lat}&lng={$lng}&username=dom_m17";
-        $response = file_get_contents($url);
-        $data = json_decode($response, true);
-
-        return $data;
-    } catch (Exception $error) {
-        echo 'Error making API call: ' . $error->getMessage();
-    }
-} -->
