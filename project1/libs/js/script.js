ee71f00b3ac020df9fffee8eac4d37f81a64ba26
dom-m17 <dominicmaynard00@gmail.com>
2024-01-18 19:52:07 +0000

    Adding elements and styling

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,9 +1,18 @@
 // Rendering the map
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13,
+    // zoomControl: false
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+map.zoomControl.setPosition('bottomright');
 
+// L.imageOverlay(
+//     "/project1/resources/title.jpeg", 
+//     [[51.49, -0.13], [51.51, -0.07]]
+//     ).addTo(map);
