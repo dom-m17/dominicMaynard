a7d817c161869d1a62aacf8b7f1be8dd6294053f
dom-m17 <dominicmaynard00@gmail.com>
2024-01-19 19:48:47 +0000

    Finishing UI

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -17,6 +17,7 @@ const standard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const streets = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
     {
+      maxZoom: 19,  
       attribution:
         "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
     }
@@ -25,17 +26,31 @@ const streets = L.tileLayer(
 const satellite = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     {
+      maxZoom: 19,  
       attribution:
         "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
     }
   );
 
+const dark = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', 
+    {
+	maxZoom: 19,  
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
+});
+
   var basemaps = {
     "Standard": standard,
     "Streets": streets,
-    "Satellite": satellite
+    "Satellite": satellite,
+    "Dark": dark
   };
 
 const layerControl = L.control.layers(basemaps).addTo(map);
 
 layerControl.setPosition('bottomleft');
+
+L.easyButton("fa-info", function (btn, map) {
+    $("#info-modal").modal("show");
+  }).addTo(map).getContainer().classList.add('custom-position');
